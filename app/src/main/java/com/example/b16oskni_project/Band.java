871765dad6bc048c414b9e26a493b6dcd2183480
Oskni <oskar.n@hotmail.com>
2020-08-09 02:14:06 +0200

    Removed unused getters/setters

--- app/src/main/java/com/example/b16oskni_project/Band.java
+++ app/src/main/java/com/example/b16oskni_project/Band.java
@@ -35,32 +35,9 @@ public class Band {
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCompany() {
-        return company;
-    }
-
-    public void setCompany(String company) {
-        this.company = company;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
+    
     public String getUrl() {
         return url;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
 }
